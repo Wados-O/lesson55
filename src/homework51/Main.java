791d1af179d5ece9c21f2689ca392ce81b95abdd
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-02 19:34:55 +0200

    feat :add Month.days

--- src/homework51/Main.java
+++ src/homework51/Main.java
@@ -5,7 +5,9 @@ public class Main {
   public static void main(String[] args) {
     Month[] allMonth = Month.values();
     Month second = Month.values()[1];
-    System.out.println("second" + second +"second.getName()" +second.getName());
+    System.out.println("second" + second +
+        "," + "second.getDays()" + second.getDays()
+        + "second.getName()" + second.getName());
   }
 
 }
