49a1b0f335e9655d037039729735c00348ea1fde
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-02 19:46:21 +0200

    feat :solve homework51 task 1

--- src/homework51/Main.java
+++ src/homework51/Main.java
@@ -1,13 +1,38 @@
 package homework51;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
     Month[] allMonth = Month.values();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите номер месяца: ");
+    while (!scanner.hasNextInt()){
+      if (!scanner.hasNext()){
+      System.err.println("Неожиданный конец файла ");
+      return;
+
+      }
+    }
+    int monthNumber = scanner.nextInt();
+    if (1 <= monthNumber && monthNumber <= allMonth.length){
+      int index = monthNumber -1;
+      Month month = allMonth[index];
+      System.out.printf("Месяц с номером %d: %s (%d дней)%n",monthNumber,month.getName(),month.getDays() );
+    }else {
+      System.out.println("Нет месяца с номером " + monthNumber);
+    }
+
+
+
+
     Month second = Month.values()[1];
-    System.out.println("second" + second +
-        "," + "second.getDays()" + second.getDays()
-        + "second.getName()" + second.getName());
+
+
+    System.out.println("second " + second +
+        ", " + "second.getDays(): " + second.getDays()
+        + " second.getName(): " + second.getName());
   }
 
 }
