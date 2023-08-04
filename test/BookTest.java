24eded02372ea307c12e8c6d6a94362439436663
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-04 11:35:04 +0200

    feat : add BookTitleAuthorComparatorTest

--- test/BookTest.java
+++ test/BookTest.java
@@ -1,25 +1,29 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import homework53.Book;
+import homework54.BookTitleAuthorComparator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class BookTest {
+
   @Test
-  public void BookTitleAuthorComparator(){
+  public void BookTitleAuthorComparator() {
     List<Book> books = new ArrayList<>();
-    Book book1 = new Book("A","A",22);
-    Book book2 = new Book("A","B",44);
-    Book book3 = new Book("B","C",55);
+    Book book1 = new Book("A", "A", 22);
+    Book book2 = new Book("A", "B", 44);
+    Book book3 = new Book("B", "C", 55);
     books.add(book1);
     books.add(book2);
     books.add(book3);
     List<Book> newBooks = new ArrayList<>();
+    books.sort(new BookTitleAuthorComparator());
     newBooks.add(book1);
     newBooks.add(book2);
     newBooks.add(book3);
+    assertEquals(newBooks, books);
   }
 
 }
