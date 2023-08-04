3d64f080d470342471b5865ce0f270bab8298f13
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-04 11:30:35 +0200

    feat : homework 55 created test Comparator Book

--- test/BookTest.java
+++ test/BookTest.java
@@ -1,8 +1,25 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import homework53.Book;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class BookTest {
+  @Test
+  public void BookTitleAuthorComparator(){
+    List<Book> books = new ArrayList<>();
+    Book book1 = new Book("A","A",22);
+    Book book2 = new Book("A","B",44);
+    Book book3 = new Book("B","C",55);
+    books.add(book1);
+    books.add(book2);
+    books.add(book3);
+    List<Book> newBooks = new ArrayList<>();
+    newBooks.add(book1);
+    newBooks.add(book2);
+    newBooks.add(book3);
+  }
 
 }
